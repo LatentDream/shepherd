1fdc746584fedb60750bd9f830649f44d3f37381
Guillaume Thibault <guillaume.thibault.98@gmail.com>
2024-02-04 15:52:51 -0500

    Using Command pattern

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,48 @@
-use core::panic;
-use std::{path::{Path, PathBuf}, thread::sleep, time};
+use std::{path::{Path, PathBuf}, process::ExitCode, env};
 use watcher::windows;
 
 mod watcher;
 
-fn main() {
-    
-    println!("The sherperd is starting his day! 🐑🐑🐑");
+struct Command {
+    name: &'static str,
+    description: &'static str,
+    run: fn(&str, env::Args) -> ExitCode,
+}
+
+const COMMANDS: &[Command] = &[
+    Command {
+        name: "help",
+        description: "Show this help",
+        run: help,
+    },
+    Command {
+        name: "watch",
+        description: "Watch a directory. Usage: watch <dir> <bool-watch-sub>",
+        run: watch,
+    },
+];
+
+fn help(program: &str, _args: env::Args) -> ExitCode {
+    usage(program);
+    ExitCode::SUCCESS
+}
+
+fn usage(program: &str) {
+    println!("");
+    eprintln!("Usage: {program} <command>");
+    eprintln!("\nCommands:");
+    for cmd in COMMANDS.iter() {
+        eprintln!("    {name} - {description}", name = cmd.name, description = cmd.description);
+    }
+}
+
+struct WatcherDog {
+    dir: PathBuf,
+    sub: bool,
+    callback: Box<dyn Fn(&Path)>,
+}
+
+fn watch_extract_params(_args: env::Args) {
     // Accept a directory as an argument
     // - Args: -d, --dir
     // - Default: current directory
@@ -22,37 +58,68 @@ fn main() {
     // Options: Ignore directories
     // - Args: -D, --dir
     // - Default: false
+    // → Insert option in WatchDog to pass it to the watcher 
+    unimplemented!()
+}
 
-    // TODO: cmd args parser
-    let args: Vec<String> = std::env::args().collect();
-    let dir = &args[1];  // ! Can panic!
-    // let sub = args[2].parse().unwrap();  // ! Can panic!
-    let watch_dog: WatcherDog = WatcherDog {
-        dir: PathBuf::from(dir),
-        sub: false,
-        callback: Box::new(display_change),
+fn dummy_fn(path: &Path) {
+    println!("{:?}", path);
+}
+
+
+fn watch(_program: &str, args: env::Args) -> ExitCode {
+    let mut args = args;
+    let dir = args.next().expect("directory or file");
+    let sub: bool = match args.next().unwrap_or_else(|| "false".to_string()).to_lowercase().as_str() {
+        "true" => true,
+        "false" => false,
+        "0" => false,
+        "1" => true,
+        _ => {
+            eprintln!("Invalid value for watch sub-tree argument. Expected true or false");
+            return ExitCode::FAILURE;
+        }
+    };
+
+    let _watch_dog: WatcherDog = WatcherDog {
+        dir: PathBuf::from(dir.clone()),
+        sub,
+        callback: Box::new(dummy_fn),
     };
 
     #[cfg(target_family = "windows")]
     {
-        windows::watch(dir, true);
+        windows::watch(&dir, true);
     }
     #[cfg(target_family = "unix")]
     {
-        unimplemented!()
         // https://www.man7.org/linux/man-pages/man7/inotify.7.html
-        // Will need to check for macOS
+        unimplemented!();
     }
-
+    ExitCode::SUCCESS
 }
 
-fn display_change(path: &Path) -> () {
-    println!("Wild sheep detected! → {:?}", path)
-}
+fn main() -> ExitCode {
 
-struct WatcherDog {
-    dir: PathBuf,
-    sub: bool,
-    callback: Box<dyn Fn(&Path) -> ()>,
-}
+    let mut args = env::args();
+    
+    let program_name = args.next().expect("🐑");
+    if let Some(command_name) = args.next() {
+        // Process cmd
+        if let Some(command) = COMMANDS.iter().find(|command| command.name == command_name) {
+            (command.run)(&program_name, args);
+        } else {
+            usage(&program_name);
+            eprintln!("ERROR: command {command_name} is unknown");
+            return ExitCode::FAILURE;
+        }
 
+    } else {
+        // Show usage
+        eprintln!("[ERROR] No command is provided");
+        usage(&program_name);
+        return ExitCode::FAILURE;
+
+    }
+    ExitCode::SUCCESS
+}
